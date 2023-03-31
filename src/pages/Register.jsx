f217cb4bc4992e9d30ab0929c689674be5a05578
jmbaik <famian@daum.net>
2023-03-31 15:24:54 +0900

    dev 007

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import Wrapper from '../assets/wrappers/RegisterPage';
+import Logo from '../components/Logo';
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Wrapper className="full-page">
+      <form className="form">
+        <Logo />
+        <h3>Login</h3>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input type="text" name="name" className="form-input" />
+        </div>
+        <button type="submit" className="btn btn-block">
+          로그인
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
